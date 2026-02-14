5edf48f00b71cacff75aa5c72336394b11007bcc
Meemkoo <guy99992@gmail.com>
2026-02-13 20:23:44 -0600

    Gut the drivetrain for the time being

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -26,26 +26,11 @@ import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.intake.Intake;
 
 public class RobotContainer {
-    private final DataLog m_log0 = DataLogManager.getLog();
-    // private final StringLogEntry testStringLog = new StringLogEntry(m_log0, "/root/teststring");
-
     private final CommandXboxController driverCtl = new CommandXboxController(0);
     private final CommandXboxController coDriverCtl = new CommandXboxController(1);
 
     // Shooter
     // private final Shooter shooter = new Shooter();
-
-
-    // Drivetrain
-    private final double maxSpeed = TunerConstantsV1Protobot.kSpeedAt12Volts.in(MetersPerSecond); // TODO: Get real max speed
-    private final double maxAngularRate = RotationsPerSecond.of(2).in(RadiansPerSecond);
-    private final SwerveRequest.FieldCentric m_driveFieldCentric = new SwerveRequest.FieldCentric()
-        .withDeadband(maxSpeed * 0.09)
-        .withRotationalDeadband(maxAngularRate * 0.09) // Experiment with best values for these but they worked on Hawksbill
-        .withDriveRequestType(com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType.OpenLoopVoltage);
-    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
-    private final Drivetrain drivetrain = TunerConstantsV1Protobot.createDrivetrain();
 
 
     // Indexer
@@ -54,57 +39,22 @@ public class RobotContainer {
     // Intake
     private final Intake intake = new Intake();
 
-
-
-    // private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    // private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
-
-    // /* Setting up bindings for necessary control of the swerve drive platform */
-    // private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-    //         .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-    //         .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
-    // private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-    // private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
-
-    // private final Telemetry logger = new Telemetry(MaxSpeed);
-
-    // private final CommandXboxController joystick = new CommandXboxController(0);
-
-
     private Logging log = new Logging();
 
-    private Locator locator = new Locator(drivetrain::getPos);
+    // private Locator locator = new Locator(drivetrain::getPos);
 
 
     public RobotContainer() {
-        DriverStation.startDataLog(m_log0);
-
         if (Robot.isSimulation()) {
             DriverStation.silenceJoystickConnectionWarning(true);
         }
 
         configureBindings();
 
-        drivetrain.registerTelemetry(log::logCTREChassis);
+        // drivetrain.registerTelemetry(log::logCTREChassis);
     }
 
     private void configureBindings() {
-        drivetrain.setDefaultCommand(
-            drivetrain.applyRequest(
-                () -> m_driveFieldCentric
-                    .withVelocityX(maxSpeed * driverCtl.getLeftX())
-                    .withVelocityY(maxSpeed * driverCtl.getLeftY())
-                    .withRotationalRate(maxAngularRate * driverCtl.getRightX())
-        ));
-
-        // driverCtl.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        driverCtl.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-driverCtl.getLeftY(), -driverCtl.getLeftX()))
-        ));
-        driverCtl.leftBumper().whileTrue(Commands.run(drivetrain::seedFieldCentric));
-
-        driverCtl.a().whileTrue(drivetrain.positionDrive(() -> locator.extentionPose));
-
         // Intake bindings
         driverCtl.x().whileTrue(intake.stow());
         driverCtl.y().whileTrue(intake.runAtSpeed(RPM.of(3000)));
